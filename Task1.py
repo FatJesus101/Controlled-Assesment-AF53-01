bf04d7f766cb2e3fe909ac097acda96dd1fe641e
Craig David <cheezyfeet101@gmail.com>
2014-06-13 15:31:13 +0100

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,6 +1,29 @@
-currencies= {Pound Sterling: 1, Euro: 1.2, US Dollar: 1.6, Japanese Yen: 200}
-c_type1 = input("What Currency are you converting from? (Pound Sterling/Euro/US Dollar/Japanese Yen)")
-c_type2 = input("What Currency are you converting to? (Pound Sterling/Euro/US Dollar/Japanese Yen)")
+currencies= {
+    "Pound Sterling": 1, 
+    "Euro": 1.2, 
+    "US Dollar": 1.6, 
+    "Japanese Yen": 200
+    }
 
-if c_type1 != "Pound Sterling" or "Euro" or "US Dollar" or "Japanese Yen":
-    c_type1 = input(Please enter a valid currency to convert from (Pound Sterling/Euro/US Dollar/Japanese Yen)")
+c_type1 = raw_input("What Currency are you converting from? GBP/EUR/USD/JPY: ")
+c_type2 = raw_input("What Currency are you converting to? GBP/EUR/USD/JPY: ")
+
+def checks(c_type1, c_type2):
+    
+    if c_type1 != "GBP" or "EUR" or "USD" or "JPY":
+        c_type1 = raw_input("Please enter a valid currency to convert from- GBP/EUR/USD/JPY: ")
+
+    if c_type2 != "GBP" or "EUR" or "USD" or "JPY":
+        c_type2 = raw_input("Please enter a valid currency to convert to- GBP/EUR/USD/JPY: ")
+
+    if c_type1 == "GBP":
+        print currencies["Pound Sterling"]
+
+    if c_type1 == "EUR":
+        print currencies["Euro"]
+
+    if c_type1 == "USD":
+        print currencies["US Dollar"]
+
+    if c_type1 == "JPY":
+        print currencies["Japanese Yen"
